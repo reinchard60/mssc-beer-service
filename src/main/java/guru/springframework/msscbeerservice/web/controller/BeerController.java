d9cc2b35e8a4848319b7624fd490176f84960e59
reinchard_lang <reinchard60@hotmail.com>
2021-06-14 21:56:37 -0500

    adding controller tests. closes #4

--- src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 public class BeerController {
 
     @GetMapping("/{beerId}")
-    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId")UUID beerId) {
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable("beerId") UUID beerId) {
 
         //todo impl
         return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
@@ -26,7 +26,7 @@ public class BeerController {
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@RequestParam("beerID") UUID beerId, @RequestBody BeerDto beerDto) {
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
 
         //todo impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
