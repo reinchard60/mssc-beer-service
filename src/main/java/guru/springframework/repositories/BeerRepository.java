a65a3aa8e6850497354805b504011797598a554e
reinchard_lang <reinchard60@hotmail.com>
2021-06-29 21:31:44 -0500

    adding beer repository closes #6

---
+++ src/main/java/guru/springframework/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package guru.springframework.repositories;
+
+import guru.springframework.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
